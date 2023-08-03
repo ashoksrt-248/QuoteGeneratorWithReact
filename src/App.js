17ac746efa66da69758fade7cac186acd841046b
Ashok Amara <109223439+ashoksrt-248@users.noreply.github.com>
2023-08-03 13:48:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+
+function getRandomQuote(quotes) {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+export default function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((json) => {
+        setQuotes(json);
+        setQuote(json[0]);
+      });
+  }, []);
+
+  function getNewQuote() {
+    setQuote(getRandomQuote(quotes));
+  }
+
+  return (
+    <main>
+      <h1>Project 2: Quote Generator</h1>
+      <section>
+        <button onClick={getNewQuote}>New Quote</button>
+        <h3>
+          <span>“</span>
+          {quote?.text}
+        </h3>
+        <i>- {quote?.author}</i>
+      </section>
+    </main>
+  );
+}
